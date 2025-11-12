72d1e34a991f9b2f20290b78689157274ab034fa
David Shustin <167934750+DavidShustin@users.noreply.github.com>
2025-11-12 17:43:31 +0000

    fixed issue with vercel insight

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,25 +1,32 @@
-//import react into the bundle
-
-
+// import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
-//import bootstrap into the bundle, dont forget to do npm install bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
-
-
-
-
-//include your index.scss file into the bundle
-import "../styles/index.css";
-
-
-//import your own components
-import Layout from "./layout";
-
-//render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
-
+// import bootstrap into the bundle
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// include your index.css
+import "../styles/index.css";
+
+// put this near the top of the file
+import { inject } from "@vercel/speed-insights";
+
+if (process.env.NODE_ENV === "production") {
+  inject(); // adds the Speed Insights script for real-user metrics
+}
+
+// import your layout
+import Layout from "./layout";
+
+// import Speed Insights
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
+// render your React application
+ReactDOM.render(
+    <>
+        <Layout />
+        {process.env.NODE_ENV === "production" && <SpeedInsights />}
+    </>,
+    document.querySelector("#app")
+);
